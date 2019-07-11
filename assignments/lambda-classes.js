5e0b8280da0ccd1fe204bf5d0169067dce0273bc
Dang Lu <dang.lu@helix.com>
2019-07-10 18:25:14 -0700

    MVP met

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,127 @@
 // CODE here for your Lambda Classes
+
+
+class People{
+	constructor(peopleAttrs){
+		this.name = peopleAttrs.name;
+		this.location = peopleAttrs.location;
+		this.age = peopleAttrs.age;
+	}
+
+	speak () {
+		console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
+	}
+}
+
+class Instructor extends People{
+	constructor(insAttrs){
+		super(insAttrs);
+		this.specialty = insAttrs.specialty;
+		this.favLanguage = insAttrs.favLanguage;
+		this.catchPhrase = insAttrs.catchPhrase;
+	}
+
+	demo(subject) {
+		console.log(`Today we are learning ${subject}.`)
+	}
+
+	grade(student, subject) {
+		console.log(`${student.name} receives a perfect score on ${subject}.`)
+	}
+}
+
+class Student extends People{
+	constructor(studAttrs){
+		super(studAttrs);
+		this.previousBackground = studAttrs.previousBackground;
+		this.className = studAttrs.className;
+		this.favSubjects = studAttrs.favSubjects;
+	}
+
+	listsSubjects(){
+		this.favSubjects.forEach((item)=>console.log(item))
+	}
+
+	PRAssignments(subject) {
+		console.log(`${this.name} has submitted a PR for ${subject}`)
+	}
+
+	sprintChallenge(subject){
+		console.log(`${this.name} has begun sprint challenge on ${subject}`)
+	}
+}
+
+// TESTING!!
+
+//Instructors
+
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
+const frank = new Instructor({
+  name: 'Frank',
+  location: 'Flintville',
+  age: 20,
+  favLanguage: 'Python',
+  specialty: 'Back-end',
+  catchPhrase: `Yoooo`
+});
+
+const brad = new Instructor({
+  name: 'Brad',
+  location: 'Bedrock',
+  age: 22,
+  favLanguage: 'Python',
+  specialty: 'Data Science',
+  catchPhrase: `TESTINGG`
+});
+
+
+
+//Students
+
+const alfred = new Student({
+  name: 'Alfred',
+  location: 'Bedrock',
+  age: 7,
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `lalalalla`,
+  previousBackground : 'studying',
+  className : 'WEBPT8',
+  favSubjects : ['Math', 'Science']
+});
+
+
+const kendrick = new Student({
+  name: 'Kendrick',
+  location: 'SF',
+  age: 18,
+  favLanguage: 'Java',
+  specialty: 'back-end',
+  catchPhrase: `Don't forget the beer`,
+  previousBackground : 'criminal',
+  className : 'WEBPT8',
+  favSubjects : ['Art', 'PE']
+});
+
+const beth = new Student({
+  name: 'Beth',
+  location: 'Bedrock',
+  age: 14,
+  favLanguage: 'Python',
+  specialty: 'Data Science',
+  catchPhrase: `Duuuuuude`,
+  previousBackground : 'basketball player',
+  className : 'WEBPT8',
+  favSubjects : ['PE', 'Science', 'Social Studies']
+});
+
+
+
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -4,6 +4,83 @@ Prototype Refactor
 
 1. Copy and paste your code or the solution from yesterday
 
+function GameObject(gameObj){
+  this.createdAt = gameObj.createdAt;
+  this.name = gameObj.name;
+  this.dimensions = gameObj.dimensions;
+};
+
+GameObject.prototype.destroy = function(){
+  console.log(`${this.name} was removed from the game.`)
+};
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+function CharacterStats(statsObj){
+  GameObject.call(this, statsObj);
+  this.healthPoints = statsObj.healthPoints;
+};
+
+CharacterStats.prototype.takeDamage = function(){
+  console.log(`${this.name} took damage.`)
+};
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+function Humanoid(humObj){
+  CharacterStats.call(this, humObj);
+  GameObject.call(this, humObj);
+  this.team = humObj.team;
+  this.weapons = humObj.weapons;
+  this.language = humObj.language;
+};
+
+Humanoid.prototype.greet = function(){
+  console.log(`${this.name} offers a greeting in ${this.language}.`);
+};
+
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+class GameObject {
+	constructor(gameObj){
+		this.createdAt = gameObj.createdAt;
+		this.name = gameObj.name;
+		this.dimensions = gameObj.dimensions;
+	}
+
+	destroy() {
+		console.log(`${this.name} was removed from the game.`)
+	}
+}
+
+class CharacterStats extends GameObject {
+	constructor(statsObj){
+		super(statsObj);
+		this.healthPoints = statsObj.healthPoints;
+	}
+
+
+	takeDamage() {
+		console.log(`${this.name} took damage.`)
+	}
+}
+
+
+class Humanoid extends CharacterStats {
+	constructor(humObj){
+		super(humObj);
+		this.team = humObj.team;
+		this.weapons = humObj.weapons;
+		this.language = humObj.language;
+	}
+
+	greet() {
+		console.log(`${this.name} offers a greeting in ${this.language}.`)
+	}
+}
+
+
+
+
